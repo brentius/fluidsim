126e686e490971e19c3c5f164a22ab1a864ddb27
brentius <brentyee28@gmail.com>
2025-11-22 11:36:52 +0000

    added solver

--- main.cs
+++ main.cs
@@ -6,12 +6,12 @@ public class Grid
     public readonly int Nx, Ny;
     public readonly int dx, dy;
     
-    public double[,] p;
-    public double[,] u;
-    public double[,] v;
+    public double[,] u, v, p;
+    public double[,] s;
+    public double[,] m;
+    public double[,] newU, newV, newM;
 
-    public Grid(int nx, int ny, double Lx, double Ly)
-    {
+    public Grid(int nx, int ny, double Lx, double Ly){
         Nx = nx;
         Ny = ny;
 
@@ -19,7 +19,55 @@ public class Grid
         dy = Ly/ny;
 
         p = new double[Nx, Ny];
-        u = new double[Nx+1, Ny];
-        v = new double[Nx, Ny+1];
+        u = new double[Nx + 1, Ny];
+        v = new double[Nx, Ny + 1];
+        s = new double[Nx, Ny];
+        m = new double[Nx, Ny];
+
+        // Helper Arrays
+        newU = new double[Nx + 1, Ny];
+        newV = new double[Nx, Ny + 1];
+        newM = new double[Nx, Ny];
+
+        //walls
+        for (int i=0; i<Nx; i++)
+        {
+            for (int j=0; j<Ny; j++)
+            {
+                if (i == 0 || i == Nx - 1 || j == 0 || j == Ny - 1) s[i, j] = 0.0;
+                else s[i, j] = 1.0;
+            }
+        }
     }
+}
+
+public class Solver
+{
+    private const int iterations = 40;
+    private const double relaxation = 1.9;
+
+    public void Simulate(Grid grid, double dt, double gravity)
+    {
+        Integrate(grid, dt, gravity);
+        //reset pressure every frame
+        Array.Clear(grid.p, grid.p.length);
+        Solve(grid);
+
+        Advect(grid, dt);
+    }
+    
+    private void Integrate(Grid g, double dt, double gravity)
+    {
+        for (int i=1; i<g.Nx-1; i++)
+        {
+            for (int j=1; j<g.Ny; j++)
+            {
+                if (g.s[i,j] !=0 && g.s[i, j-1] != 0)
+                {
+                    g.v[i,j] += gravity * dt;
+                }
+            }
+        }
+    }
+
 }
